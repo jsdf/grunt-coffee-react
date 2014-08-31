4a5c1835b368f6c55e9cfa1efdc5ace4a8ef6cc6
James Friend <james@jsdf.co>
2014-08-31 22:17:22 +0800

    added update script

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+git pull origin master
+DEPENDENCY="coffee-react"
+VERSION=`npm view ${DEPENDENCY} version`
+echo "updating to $VERSION"
+npm install --save "${DEPENDENCY}@${VERSION}"
+npm test
+git add ./package.json
+git commit -m "updated ${DEPENDENCY} to v${VERSION}"
+npm version $VERSION
+read -p "will publish $VERSION. are you sure? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  git push origin master
+  npm publish .
+fi
