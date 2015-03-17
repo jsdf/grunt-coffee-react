7d2471b2f0e26f163e5995117e56374f7c98bd7d
James Friend <james@jsdf.co>
2015-03-18 09:24:15 +1100

    updated update script

--- update.sh
+++ update.sh
@@ -2,17 +2,18 @@
 set -e
 git pull origin master
 DEPENDENCY="coffee-react"
-VERSION=`npm view ${DEPENDENCY} version`
+VERSION=$(npm view ${DEPENDENCY} version)
 echo "updating to $VERSION"
 npm install --save "${DEPENDENCY}@${VERSION}"
 npm test
 git add ./package.json
 git commit -m "updated ${DEPENDENCY} to v${VERSION}"
-npm version $VERSION
+npm version "$VERSION"
 read -p "will publish $VERSION. are you sure? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   git push origin master
+  git push --tags
   npm publish .
 fi
